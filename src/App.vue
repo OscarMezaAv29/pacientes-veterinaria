3af69cf03f5bb42d38c4e85c048ef7968e53b32b
Oscar Meza <omeza@globalsoft.email>
2024-08-22 10:01:39 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Formulario from './components/Formulario.vue';
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div class="container mx-auto mt-20">
+    <Header />
+
+    <div class="mt-12 md:flex">
+      <Formulario />
+    </div>
+  </div>
+</template>
